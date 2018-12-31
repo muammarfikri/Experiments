e0c4d24c2e817e2605250d810239b8aaa0e6619a
MF <vastolorde77@gmail.com>
2018-12-31 07:57:54 +0700

    add transitions

--- app/src/main/java/com/example/mf/experiments/MainActivity.kt
+++ app/src/main/java/com/example/mf/experiments/MainActivity.kt
@@ -5,21 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val fragmentPagerAdapter = CardFragmentPagerAdapter(supportFragmentManager)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initPager()
-
-
-    }
-
-    private fun initPager() {
-        pager.adapter = fragmentPagerAdapter
-        pager.offscreenPageLimit = fragmentPagerAdapter.count
-        pager.currentItem = 1
-        pager.setPageTransformer(false, PopOutPageTransformer(pager, fragmentPagerAdapter))
     }
 }
