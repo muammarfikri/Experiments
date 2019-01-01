bcfca8f3ef1e0d6fc4ae18efb86e13674a58e005
MF <vastolorde77@gmail.com>
2019-01-01 17:12:04 +0700

    50% done

--- app/src/main/java/com/example/mf/experiments/MainActivity.kt
+++ app/src/main/java/com/example/mf/experiments/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.mf.experiments
 
+import android.graphics.BitmapFactory
 import android.graphics.Matrix
+import android.graphics.Point
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.ImageView
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.palette.graphics.Palette
 import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.ab_activity.*
 
 class MainActivity : AppCompatActivity() {
     private val matrix = Matrix()
+    lateinit var color1 : Palette.Swatch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ab_activity)
+        Palette.from(
+            BitmapFactory.decodeResource(this.resources,
+                R.drawable.spider)).generate {
+            color1 = it!!.dominantSwatch!!
+            collapser.contentScrim = ColorDrawable(color1.titleTextColor)
+            val drawable = ColorDrawable(color1.titleTextColor)
+            drawable.alpha = 255
+            addMovie.background = drawable
+            addMovie.alpha = 1f
+        }
         appbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
 //            val range = appBarLayout.totalScrollRange - iconIV.top
 //            iconIV.alpha = (1.0f - (verticalOffset/2) / range)
@@ -23,21 +38,27 @@ class MainActivity : AppCompatActivity() {
             val a = -verticalOffset
             val b = iconIV.top
             val c = appBarLayout.totalScrollRange
-            fun scale(start:Int, scaleStartX:Int, scaleEndX:Int, startOffset: Int = 0,endOffset:Int  = 0) : Float{
-//                val startX = scaleStartX
-//                val endX= scaleEndX
-//                val d = endX - scaleStartX
-//                val e = -d
-//                val f = c - a
-//                val result = -f.toFloat() / e.toFloat()
-//                Log.d("MAMA","$result")
-//                return result
-                return 0f
+            val vg = tLayout as ViewGroup
+            val toolbar = vg.getChildAt(0)
+            val bottomX = toolbar.bottom
+//            Log.d("MAMA","$a $bottomX")
+            val scale = a.toFloat() / c.toFloat()
+            val scale2 = if(a > bottomX) {
+                ((a.toFloat() - bottomX) / (c.toFloat() - bottomX) * 2)
+            }else{
+                0f
             }
-            Log.d("MAMA","$verticalOffset $c")
-            iconIV.alpha = 1f - (a.toFloat() / c.toFloat())
-            imageView2.scaleX = 1f + ((a.toFloat() / c.toFloat())/2)
-            imageView2.scaleY = 1f + ((a.toFloat() / c.toFloat())/2)
+            val scale3 = ((a.toFloat()) / (c.toFloat())) * 2
+            val point = Point()
+            windowManager.defaultDisplay.getSize(point)
+            Log.d("MAMA","${point.x}")
+            detail_content.translationY = a.toFloat() / 2
+            poster.translationX = scale2
+            detail_content.alpha = 1f - scale2
+            poster.scaleX = 1f + (scale /2)
+            poster.scaleY = 1f + (scale /2)
+            tLayout.alpha = scale
+            play.alpha = 1f - (scale * 2)
         })
     }
 
